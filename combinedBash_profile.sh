71a0f62262ca9bf0a5b1b579af051f678616d0d1
Jonathan Smalls <jonathan@smalls.cc>
2014-10-08 11:53:21 -0400

    still need this thing for remote deployment w options are for OSX

--- combinedBash_profile.sh
+++ combinedBash_profile.sh
@@ -7,8 +7,33 @@ if [ -f ~/.drush_bashrc ] ; then
 	. ~/bin/drush.complete.sh
 fi
 # https://wiki.archlinux.org/index.php/Color_Bash_Prompt
-source ~/.bashConfig/contactColor.sh
-source ~/.bashConfig/gitBranch.sh
+function contactColor {
+	szString=$1
+	szString=${szString##*/}
+	if [ $1 != $PWD ]; then
+		szString=$($1)
+	fi
+	szString=${szString:0:1}
+	case $szString in
+		[ab1AB]) colName='0;31';;
+		[cd2CD]) colName='0;33';;
+		[ef3EF]) colName='0;34';;
+		[gh4GH]) colName='0;35';;
+		[ij5IJ]) colName='0;36';;
+		[kl6KL]) colName='0;37';;
+		[mn7MN]) colName='1;30';;
+		[op8OP]) colName='1;31';;
+		[qr9QR]) colName='1;32';;
+		[st~ST]) colName='1;33';;
+		[uvEV]) colName='1;34';;
+		[wxWX]) colName='1;35';;
+		[yzYZ]) colName='1;36';;
+	esac
+	echo $colName
+}
+function gitBranch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
+}
 PS1='$(date +%a:%H%M) \[\e[$(contactColor whoami)m\]\u@\[\e[$(contactColor hostname)m\]\h:\[\e[$(contactColor $PWD)m\]\w\[\e[1;35m\]$(gitBranch) \[\e[m\]'
 
 alias emerge='emerge --ask --verbose'
