8ca7caf46a8b99a18b09a555856a037a20a0ac94
Jonathan Smalls <jonathan@sametz.com>
2014-12-12 10:37:49 -0500

    durrhh, some one else probably already did it http://www.commandlinefu.com/commands/view/7227/retrieve-a-random-command-from-the-commandlinefu.com-api

--- bashConfig/commandlinefu.sh
+++ bashConfig/commandlinefu.sh
@@ -3,9 +3,4 @@
 # Get ideas from across the internet whenever you input
 # a command, and become a command line master.
 
-#nLine=$(( RANDOM%$( wc -l $aryList )+1 ))
-#aryList=$( head -$(nLine) | tail -2 )
-szLast=!:0
-szLast=$( echo -n $szLast | openssl base64 )
-echo $szLast
-#curl -s "http://www.commandlinefu.com/commands/matching/NULL/$szLast/plaintext" | grep -A1 '^#' | grep -v commandlinefu.com | head -$(( RANDOM%25+1 )) | tail -2
+wget -qO - http://www.commandlinefu.com/commands/random/plaintext | sed -n '1d; /./p'
