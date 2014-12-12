e147812e68e1e2eb9bfd9f5de8baccc6dfd2013d
Jonathan Smalls <jonathan@sametz.com>
2014-12-12 09:26:01 -0500

    writting command to query site for command line hints

--- bashConfig/commandlinefu.sh
+++ bashConfig/commandlinefu.sh
@@ -3,4 +3,6 @@
 # Get ideas from across the internet whenever you input
 # a command, and become a command line master.
 
-curl "http://www.commandlinefu.com/commands/browse/sort-by-votes/plaintext" | grep '#'
+#nLine=$(( RANDOM%$( wc -l $aryList )+1 ))
+#aryList=$( head -$(nLine) | tail -2 )
+curl -s "http://www.commandlinefu.com/commands/browse/sort-by-votes/plaintext" | grep -A1 '^#' | grep -v commandlinefu.com | head -$(( RANDOM%25+1 )) | tail -2
