4497823aae30e9d41d9da5cc485e87be0e7cfbe4
Jonathan Smalls <jon@smalls.rocks>
2015-06-09 11:38:05 -0400

    still exploring key forwarding, but decided to make it deliberate   http://unixhelp.ed.ac.uk/CGI/man-cgi?ssh+1 delete combined preferences, because why not upload directly to home directory?

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash --
 # Remove existing files from home directory.
 rm --recursive ~/.bash_profile ~/.bashrc ~/.vim ~/.vimrc ~/.my.cnf
-
+-
 # Link to the repository for easy upgrades.
 aryLinks=( 'bash_profile' 'vimrc' 'my.cnf' 'bin' )
 for szLink in "${aryLinks[@]}"; do
-	ln --symbolic $PWD/$szLink ~/.$szLink
+       ln --symbolic $PWD/$szLink ~/.$szLink
 done
-
+-
 # Install Bower components.
 bower install &&
-	vim -c "PlugInstall"
+vim -c "PlugInstall"
+
