0ec1933ee1435f790716b3c0f83339ce67a2caba
Jonathan Smalls <jon@smalls.rocks>
2015-11-02 16:18:37 -0500

    cd function, because I usually want to see contents when changing directories

--- bash_profile.sh
+++ bash_profile.sh
@@ -22,6 +22,11 @@ EDITOR='vim'
 alias ":e"='vim'
 alias ":x"='exit'
 alias bc="bc -l"
+function cdls(){
+	cd $@
+	ls -Ah --color=always
+}
+alias cd='cdls'
 alias dd="dd bs=1024k"
 alias emerge=' emerge --ask --autounmask --update --alphabetical --quiet --deep --newuse'
 function genpass(){
