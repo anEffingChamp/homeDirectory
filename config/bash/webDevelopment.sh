5f64d88f7cea0e250c47eac62616df2953d66265
Jonathan Smalls <jon@smalls.rocks>
2017-10-13 08:12:27 -0400

    feature: wrote alias to diff over SSH
    We had some publication issues recently, so I asked my team lead to create an
    my account on the live server. Now I can proof published code, and identify
    issues with the senior members of the team.
    
    reference:
    https://serverfault.com/questions/59140/how-do-diff-over-ssh

--- config/bash/webDevelopment.sh
+++ config/bash/webDevelopment.sh
@@ -1,6 +1,6 @@
 alias csslint='csslint --ignore=box-model'
 alias ifconfig='curl ifconfig.me'
-alias haml='haml.ruby2.2 -rhaml/filters/php' 
+alias haml='haml.ruby2.2 -rhaml/filters/php'
 # http://www.hpl.hp.com/research/linux/httperf/httperf-man-0.9.txt
 alias httperf='httperf --hog --num-conns=100 --retry-on-failure --timeout=3'
 alias mosh='mosh --predict=always'
@@ -13,3 +13,12 @@ alias phpcs='phpcs --standard=Zend'
 alias piwik='php ~/webRoot/piwik/console'
 alias sass-update='sass --update --style compact .'
 alias tidy='tidy5 --errors --gnu-emacs yes'
+# serverDiff() takes a file path to an argument. It assumes that the file is
+# also available at a corresponding path on the beta, and live servers.
+# Currently it points to NOAA servers, although this is easy enough to change.
+function serverDiff()
+{
+    filePath='~/webRoot/'
+    vimdiff $filePath/$1 scp://beta/$filePath/$1 scp://production/$filePath/$1
+}
+alias vimdiff='serverDiff'
