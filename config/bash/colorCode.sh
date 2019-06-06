029740b5218562dc8b8f94e3f1dc954decef8a5a
Jonathan Smalls <jon@smalls.rocks>
2019-06-06 13:31:27 -0400

    feature: using 256 colors in colorCode()
    reference:
    https://unix.stackexchange.com/questions/92447/bash-script-to-get-ascii-values-for-alphabet

--- config/bash/colorCode.sh
+++ config/bash/colorCode.sh
@@ -14,35 +14,10 @@ function colorCode {
         string=${string:0:1}
     fi
     string=${string:0:1}
-    # TODO How can I better use color?
-    # https://unix.stackexchange.com/questions/124407/what-color-codes-can-i-use-in-my-ps1-prompt
-    case $string in
-        [ab1AB]) color='0;31';;
-        #[ab1AB]) color="$RED";;
-        [cd2CD]) color='0;33';;
-        #[cd2CD]) color="$GREEN";;
-        [ef3EF]) color='0;34';;
-        #[ef3EF]) color="$BLUE";;
-        [gh4GH]) color='0;35';;
-        #[gh4GH]) color="$PURPLE";;
-        [ij5IJ]) color='0;36';;
-        #[ij5IJ]) color="$CYAN";;
-        [kl6KL]) color='0;37';;
-        #[kl6KL]) color="$BOLD";;
-        [mn7MN]) color='1;30';;
-        #[mn7MN]) color="$BOLD_GRAY";;
-        [op8OP]) color='1;31';;
-        #[op8OP]) color="$BOLD_RED";;
-        [qr9QR]) color='1;32';;
-        #[qr9QR]) color="$BOLD_GREEN";;
-        [st0ST]) color='1;33';;
-        #[st0ST]) color="$YELLOW";;
-        [uv~EV]) color='1;34';;
-        #[uv~EV]) color="$BOLD_BLUE";;
-        [wxWX]) color='1;35';;
-        #[wxWX]) color="$PINK";;
-        [yzYZ]) color='1;36';;
-        #[yzYZ]) color="$BOLD_CYAN";;
-    esac
-    echo "\\033[${color}m"
+    color=$((`printf '%d' "'${string}"` - 28))
+    # At this point $color is actually a number, so lets give it some range in
+    # the available 256 palette. The meaningful ASCII characters are in
+    # [48,127], so lets increment in a meaningful way.
+    color=$(($color * 255 / (127 - 48)))
+    echo "\\033[38;5;${color}m"
 }
