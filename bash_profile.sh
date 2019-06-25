22234d53c52581e8e5816a294b5f45ab17943bc3
Jonathan Smalls <jon@smalls.rocks>
2019-06-25 11:02:21 -0400

    bug: improved condition for fortune when unavailable
    reference:
    https://stackoverflow.com/questions/229551/how-to-check-if-a-string-contains-a-substring-in-bash

--- bash_profile.sh
+++ bash_profile.sh
@@ -78,7 +78,7 @@ if [ -f ~/.config/bash/webDevelopment.sh ]; then
 fi
 
 # startup routine
-if [ '' != $(which fortune) ]; then
+if [ 'no fortune in' != *"$(which fortune)"* ]; then
     echo -e "$(colorCode $(hostname))$(fortune)\033[m\n"
 fi
 # Run upgrades on Thursdays, because no one wants to deal with broken systems on
