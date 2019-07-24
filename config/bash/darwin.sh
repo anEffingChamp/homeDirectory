3c417cc3427c2da174ec050b4393d918f622253e
Jonathan Smalls <jon@smalls.rocks>
2019-07-24 13:33:46 -0400

    feature: added httpd alias for OSX
    reference:
    https://www.garron.me/en/bits/mac-os-x-apache-start-stop-restart.html

--- config/bash/darwin.sh
+++ config/bash/darwin.sh
@@ -1,23 +1,24 @@
 if [ $(uname) == 'Darwin' ]; then
-	function functionCD(){
-		\cd "$@"
-		\ls -GA
-	}
+    function functionCD(){
+        \cd "$@"
+        \ls -GA
+    }
     function functionFind(){
         \find -L . -iname *$@*
     }
-	function functionSupergenpass(){
-		echo -n "Enter password. ";
-		stty -echo; read szPass; stty echo; echo
+    function functionSupergenpass(){
+        echo -n "Enter password. ";
+        stty -echo; read szPass; stty echo; echo
         pbcopy <<< $(\supergenpass -p $szPass "$@")
-	}
-	alias cd='functionCD'
+    }
+    alias cd='functionCD'
     alias chmod='chmod -vv'
-	alias ls='ls -GA'
+    alias ls='ls -GA'
+    alias httpd='sudo /usr/sbin/apachectl'
     alias open='\open'
-	alias ps='ps axjSm'
-	alias top='top -l 1 -o mem -stats pid,user,command,cpu,mem -n 10'
-	alias w='w -i'
+    alias ps='ps axjSm'
+    alias top='top -l 1 -o mem -stats pid,user,command,cpu,mem -n 10'
+    alias w='w -i'
 # shutdown
     poweroff='sudo shutdown -h 60'
 fi
